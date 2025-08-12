3d8c3bd4167e596a5873211c0aaacea1ac803967
lucasugb <2024101541@academicougb.com.br>
2025-08-12 19:51:22 -0300

    acrescentado else

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int idade = 24;
 Console.WriteLine("A idade é" + idade);
-if (idade >= 18)
-{
+if (idade >= 18) 
     Console.WriteLine("Você pode tirar habilitação");
-}
+
+else
+    Console.WriteLine("Você não pode tirar habilitação");
 
