220ed169e89ddb5c42c35ed4ea9f99031911d105
lucasugb <2024101541@academicougb.com.br>
2025-08-19 19:37:59 -0300

    Copiei do prof pra ficar igual ))

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,46 +1,20 @@
-﻿using System.ComponentModel.Design;
-
-Console.WriteLine("Informe seu nome: ");
+﻿Console.WriteLine("Informe seu nome: ");
 string nome = Console.ReadLine();
-
 Console.WriteLine("Informe seu salário: ");
 decimal salario = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Seja bem-vinda(o) {0}, o salário informado foi {1:C3}", nome, salario);
 
-Console.WriteLine($"Seu nome é {nome} e seu salário é R${salario}");
+//informem o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto de IRPF
+decimal irpfDevido;
+if (salario <= 2259.20m)
+    irpfDevido = 0;
+else if (salario <= 2826.65m)
+    irpfDevido = salario * 0.075m - 169.21m;
+else if (salario <= 3751.05m)
+    irpfDevido = salario * 0.15m - 381.44m;
+else if (salario <= 4664.68m)
+    irpfDevido = salario * 0.225m - 662.77m;
+else
+    irpfDevido = salario * 0.275m - 896m;
 
-decimal faixa1 = 2259.20m;
-decimal faixa2 = 2826.65m;
-decimal faixa3 = 3751.05m;
-decimal faixa4 = 4664.68m;
-decimal faixa5 = 4664.49m;
-
-
-if (salario <= faixa1)
-{
-    Console.WriteLine("Você está isento de imposto");
-}
-
-else if (salario <= faixa2)
-{
-    double irps = (double)salario * 0.075 - 169.41;
-    Console.WriteLine(irps);
-}
-
-else if (salario <= faixa3)
-{
-    double irps = (double)salario * 0.15 - 381.44;
-    Console.WriteLine(irps);
-}
-
-else if (salario <= faixa4)
-{
-    double irps = (double)salario * 0.225 - 662.77;
-    Console.WriteLine(irps);
-}
-
-else if (salario > faixa5)
-{
-    double irps = (double)salario * 0.275 - 896;
-    Console.WriteLine(irps);
-}
-
+Console.WriteLine($"Para o salário de {salario} o valor a ser pago de IRPF é {irpfDevido}, então o salário com desconto de IRPF será {salario - irpfDevido}");
