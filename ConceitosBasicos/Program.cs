324a8bee6df717692124aed549867479019669e0
lucasugb <2024101541@academicougb.com.br>
2025-08-19 19:21:33 -0300

    Adicionei a alíquota lá ou que quer que seja

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -12,7 +12,7 @@ decimal faixa1 = 2259.20m; //se menor ou igual = isento
 decimal faixa2 = 2826.65m; //De 2.259,21 até 2.826,65: 7,5%
 decimal faixa3 = 3751.05m; //De 2.826,66 até 3.751,05: 15%
 decimal faixa4 = 4664.68m; //De 3.751,07 até 4.664,68: 22,5%
-decimal faixa5 = 4664.49m ; //Acima de R$ 4.664,68: 27,5%
+decimal faixa5 = 4664.49m; //Acima de R$ 4.664,68: 27,5%
 
 
 if (salario <= faixa1)
@@ -22,28 +22,25 @@ if (salario <= faixa1)
 
 else if (salario <= faixa2)
 {
-    double irps = (double)salario * 0.075;
+    double irps = (double)salario * 0.075 - 169.41;
     Console.WriteLine(irps);
 }
 
 else if (salario <= faixa3)
 {
-    double irps = (double)salario * 0.15;
+    double irps = (double)salario * 0.15 - 381.44;
     Console.WriteLine(irps);
 }
 
 else if (salario <= faixa4)
 {
-    double irps = (double)salario * 0.225;
+    double irps = (double)salario * 0.225 - 662.77;
     Console.WriteLine(irps);
 }
 
 else if (salario > faixa5)
 {
-    double irps = (double)salario * 0.275;
+    double irps = (double)salario * 0.275 - 896;
     Console.WriteLine(irps);
 }
-
-
-
 
